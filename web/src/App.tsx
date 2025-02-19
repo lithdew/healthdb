c2993f56f7a16f2f04d9e04d6bce4cf2d2c6e636
lithdew <kenta@lithdew.net>
2025-02-19 18:05:25 +0800

    feat: add textarea

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <>
-      <div className="bg-red-500">Hello</div>
-    </>
+    <div className="h-dvh w-full bg-gray-50 flex flex-col">
+      <div className="p-4 grow">Hello world</div>
+      <div className="p-4">
+        <textarea
+          className="p-6 rounded-md bg-gray-200 w-full"
+          rows={4}
+          placeholder="What's on your mind?"
+        />
+      </div>
+    </div>
   );
 }
 
