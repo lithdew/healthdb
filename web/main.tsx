ea1820dfe9df5828ad0f5b2e48c52f4cf6177a96
lithdew <kenta@lithdew.net>
2025-02-20 02:56:50 +0800

    fix: remove old providers code

--- web/main.tsx
+++ web/main.tsx
@@ -1,25 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./app.tsx";
-import { MemoryStoreProvider } from "./lib/memory.tsx";
-import { DBProvider } from "./lib/db.tsx";
-import { GlobalStoreProvider } from "./store.tsx";
-
-const Providers = ({ children }: React.PropsWithChildren) => {
-  return (
-    <DBProvider>
-      <GlobalStoreProvider>
-        <MemoryStoreProvider>{children}</MemoryStoreProvider>
-      </GlobalStoreProvider>
-    </DBProvider>
-  );
-};
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    <App />
   </StrictMode>
 );
